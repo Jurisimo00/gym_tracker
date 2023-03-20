22515147a8268dfb7789a468e68c1a53f0c7819e
Jurisimo00 <juri.simoncini00@studio.unibo.it>
2023-03-20 16:00:45 +0100

    clean code and fix stream and video utils

--- opencv_object_tracker.py
+++ opencv_object_tracker.py
@@ -10,6 +10,8 @@ import numpy as np
 import holi
 import PoseTracking
 from WebcamMultiThread import WebcamStream
+import streamUtils as su
+import videoUtils as vu
 
 def track():
 	# construct the argument parser and parse the arguments
@@ -42,120 +44,115 @@ def track():
 	# grab the appropriate object tracker using our dictionary of
 	# OpenCV object tracker objects
 		tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
-		# initialize the bounding box coordinates of the object we are going
-# to track
-	initBB = None
-	first = True
-	pose = True
+
 # if a video path was not supplied, grab the reference to the web cam
 	if not args.get("video", False):
 		print("[INFO] starting video stream...")
 		#vs = VideoStream(src=0).start()
-		webcam_stream=WebcamStream(stream_id=0)
-		webcam_stream.start()
+		#webcam_stream=WebcamStream(stream_id=0)
+		#webcam_stream.start()
 		#flag to know if is a stream or not
 		webcam = True
 		time.sleep(1.0)
+		su.stream(tracker)
 # otherwise, grab a reference to the video file
 	else:
 		vs = cv2.VideoCapture(args["video"])
 		#vs = FileVideoStream(args["video"]).start()
 		time.sleep(1.0)
 		webcam = False
+		vu.video(tracker,args)
 # initialize the FPS throughput estimator
-	fps = None
-	# loop over frames from the video stream
-	while True:
-		# grab the current frame, then handle if we are using a
-		# VideoStream or VideoCapture object
-		frame = vs.read() if(not webcam) else webcam_stream.read()
-		#frame = webcam_stream.read()
-		frame = frame[1] if args.get("video", False) else frame
-		# check to see if we have reached the end of the stream
-		if frame is None:
-			break
-		# resize the frame (so we can process it faster) and grab the
-		# frame dimensions
-		frame = imutils.resize(frame, width=500)
-		(H, W) = frame.shape[:2]
-		#frame = holi.process(frame)
-		if pose:
-			frame, land = PoseTracking.process(frame)
-			a = np.array([int(land[23].x*W),int(land[23].y*H)])
-			b = np.array([int(land[25].x*W),int(land[25].y*H)])
-			c = np.array([int(land[27].x*W),int(land[27].y*H)])
-			cv2.circle(frame, (a[0], a[1]), 12,
-				(0, 255, 0), 2)
-			check, angle = PoseTracking.getAngle(a,b,c)
-#	#print(angle)
-			if(check):
-				cv2.putText(frame, f'{int(angle)}',(b[0],b[1]),
-	  					cv2.FONT_HERSHEY_SIMPLEX,0.6, (0, 0, 255), 2)
-		# check to see if we are currently tracking an object
-		if initBB is not None:
-			# grab the new bounding box coordinates of the object
-			(success, box) = tracker.update(frame)
-			# check to see if the tracking was a success
-			if success:
-				(x, y, w, h) = [int(v) for v in box]
-				cv2.rectangle(frame, (x, y), (x + w, y + h),
-					(0, 255, 0), 2)
-				centerX = np.rint((x + (w/2))).astype(int)
-				centerY = np.rint((y + (y/2))).astype(int)
-				#print(centerX,centerY)
-				if first:
-					points = np.array([[centerX,centerY]],dtype=np.uint8)
-					first=False
-				points = np.append(points,[[centerX,centerY]], axis=0)
-				cv2.polylines(frame, 
-				[points], 
-				isClosed = False,
-				color = (0,255,0),
-				thickness = 3, 
-				lineType = cv2.LINE_AA)
-			# update the FPS counter
-			fps.update()
-			fps.stop()
-			# initialize the set of information we'll be displaying on
-			# the frame
-			info = [
-				("Tracker", args["tracker"]),
-				("Success", "Yes" if success else "No"),
-				("FPS", "{:.2f}".format(fps.fps())),
-			]
-			# loop over the info tuples and draw them on our frame
-			for (i, (k, v)) in enumerate(info):
-				text = "{}: {}".format(k, v)
-				cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-		# show the output frame
-		#frame = cv2.flip(frame,1)
-		cv2.imshow("Frame", frame)
-		key = cv2.waitKey(1) & 0xFF
-		# if the 's' key is selected, we are going to "select" a bounding
-		# box to track
-		if key == ord("s"):
-			# select the bounding box of the object we want to track (make
-			# sure you press ENTER or SPACE after selecting the ROI)
-			initBB = cv2.selectROI("Frame", frame, fromCenter=False,
-				showCrosshair=True)
-			#when track movement don't use poseTracking
-			pose = False
-			# start OpenCV object tracker using the supplied bounding box
-			# coordinates, then start the FPS throughput estimator as well
-			tracker.init(frame, initBB)
-			fps = FPS().start()
-			
-				# if the `q` key was pressed, break from the loop
-		elif key == ord("q"):
-			break
-	# if we are using a webcam, release the pointer
-	if not args.get("video", False):
-		webcam_stream.stop()
-	# otherwise, release the file pointer
-	else:
-		#vs.stop()
-		vs.release()
-	# close all windows
-	cv2.destroyAllWindows()
+		fps = None
+		# loop over frames from the video stream
+	# 	while True:
+	# 		# grab the current frame, then handle if we are using a
+	# 		# VideoStream or VideoCapture object
+	# 		time.sleep(0.1)
+	# 		frame = vs.read()
+	# 		#frame = webcam_stream.read()
+	# 		frame = frame[1] if args.get("video", False) else frame
+	# 		# check to see if we have reached the end of the stream
+	# 		if frame is None:
+	# 			break
+	# 		# resize the frame (so we can process it faster) and grab the
+	# 		# frame dimensions
+	# 		frame = imutils.resize(frame, width=500)
+	# 		(H, W) = frame.shape[:2]
+	# 		#frame = holi.process(frame)
+	# 		if pose:
+	# 			frame, land = PoseTracking.process(frame)
+	# 			a = np.array([int(land[23].x*W),int(land[23].y*H)])
+	# 			b = np.array([int(land[25].x*W),int(land[25].y*H)])
+	# 			c = np.array([int(land[27].x*W),int(land[27].y*H)])
+	# 			cv2.circle(frame, (a[0], a[1]), 12,
+	# 				(0, 255, 0), 2)
+	# 			check, angle = PoseTracking.getAngle(a,b,c)
+	# #	#print(angle)
+	# 			if(check):
+	# 				cv2.putText(frame, f'{int(angle)}',(b[0],b[1]),
+	# 						cv2.FONT_HERSHEY_SIMPLEX,0.6, (0, 0, 255), 2)
+	# 		# check to see if we are currently tracking an object
+	# 		if initBB is not None:
+	# 			# grab the new bounding box coordinates of the object
+	# 			(success, box) = tracker.update(frame)
+	# 			# check to see if the tracking was a success
+	# 			if success:
+	# 				(x, y, w, h) = [int(v) for v in box]
+	# 				cv2.rectangle(frame, (x, y), (x + w, y + h),
+	# 					(0, 255, 0), 2)
+	# 				centerX = np.rint((x + (w/2))).astype(int)
+	# 				centerY = np.rint((y + (y/2))).astype(int)
+	# 				#print(centerX,centerY)
+	# 				if first:
+	# 					points = np.array([[centerX,centerY]],dtype=np.uint8)
+	# 					first=False
+	# 				points = np.append(points,[[centerX,centerY]], axis=0)
+	# 				cv2.polylines(frame, 
+	# 				[points], 
+	# 				isClosed = False,
+	# 				color = (0,255,0),
+	# 				thickness = 3, 
+	# 				lineType = cv2.LINE_AA)
+	# 			# update the FPS counter
+	# 			fps.update()
+	# 			fps.stop()
+	# 			# initialize the set of information we'll be displaying on
+	# 			# the frame
+	# 			info = [
+	# 				("Tracker", args["tracker"]),
+	# 				("Success", "Yes" if success else "No"),
+	# 				("FPS", "{:.2f}".format(fps.fps())),
+	# 			]
+	# 			# loop over the info tuples and draw them on our frame
+	# 			for (i, (k, v)) in enumerate(info):
+	# 				text = "{}: {}".format(k, v)
+	# 				cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
+	# 					cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+	# 		# show the output frame
+	# 		#frame = cv2.flip(frame,1)
+	# 		cv2.imshow("Frame", frame)
+	# 		key = cv2.waitKey(1) & 0xFF
+	# 		# if the 's' key is selected, we are going to "select" a bounding
+	# 		# box to track
+	# 		if key == ord("s"):
+	# 			# select the bounding box of the object we want to track (make
+	# 			# sure you press ENTER or SPACE after selecting the ROI)
+	# 			initBB = cv2.selectROI("Frame", frame, fromCenter=False,
+	# 				showCrosshair=True)
+	# 			#when track movement don't use poseTracking
+	# 			pose = False
+	# 			# start OpenCV object tracker using the supplied bounding box
+	# 			# coordinates, then start the FPS throughput estimator as well
+	# 			tracker.init(frame, initBB)
+	# 			fps = FPS().start()
+				
+	# 				# if the `q` key was pressed, break from the loop
+	# 		elif key == ord("q"):
+	# 			break
+
+	# 	#vs.stop()
+	# 	vs.release()
+	# 	# close all windows
+	# 	cv2.destroyAllWindows()
 
--- videoUtils.py
+++ videoUtils.py
@@ -8,35 +8,102 @@ import time
 import cv2
 import PoseTracking
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", required=True,
-	help="path to input video file")
-args = vars(ap.parse_args())
-# start the file video stream thread and allow the buffer to
-# start to fill
-print("[INFO] starting video file thread...")
-fvs = FileVideoStream(args["video"]).start()
-time.sleep(1.0)
-# start the FPS timer
-fps = FPS().start()
-# loop over frames from the video file stream
-while fvs.more():
-	# grab the frame from the threaded video file stream, resize
-	# it, and convert it to grayscale (while still retaining 3
-	# channels)
-	frame = fvs.read()
-	frame = imutils.resize(frame, width=450)
-	frame,_= PoseTracking.process(frame)
-	#frame = np.dstack([frame, frame, frame])	
-	# show the frame and update the FPS counter
-	cv2.imshow("Frame", frame)
-	cv2.waitKey(1)
-	fps.update()
-	# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-# do a bit of cleanup
-cv2.destroyAllWindows()
-fvs.stop()
+def video(tracker,args):
+    initBB = None
+    first = True
+    pose = True
+    # construct the argument parse and parse the arguments
+    # start the file video stream thread and allow the buffer to
+    # start to fill
+    print("[INFO] starting video file thread...")
+    fvs = FileVideoStream(args["video"]).start()
+    time.sleep(1.0)
+    # start the FPS timer
+    fps = FPS().start()
+    # loop over frames from the video file stream
+    while fvs.more():
+        # grab the frame from the threaded video file stream, resize
+        # it, and convert it to grayscale (while still retaining 3
+        # channels)
+        frame = fvs.read()
+        frame = imutils.resize(frame, width=450)
+        (H, W) = frame.shape[:2]
+        if pose:
+            frame, land = PoseTracking.process(frame)
+            a = np.array([int(land[23].x*W),int(land[23].y*H)])
+            b = np.array([int(land[25].x*W),int(land[25].y*H)])
+            c = np.array([int(land[27].x*W),int(land[27].y*H)])
+            cv2.circle(frame, (a[0], a[1]), 12,
+                (0, 255, 0), 2)
+            check, angle = PoseTracking.getAngle(a,b,c)
+            if(check):
+                cv2.putText(frame, f'{int(angle)}',(b[0],b[1]),
+                        cv2.FONT_HERSHEY_SIMPLEX,0.6, (0, 0, 255), 2)
+        #frame = np.dstack([frame, frame, frame])	
+        # show the frame and update the FPS counter
+        cv2.waitKey(1)
+        fps.update()
+        # check to see if we are currently tracking an object
+        if initBB is not None:
+            # grab the new bounding box coordinates of the object
+            (success, box) = tracker.update(frame)
+            # check to see if the tracking was a success
+            if success:
+                (x, y, w, h) = [int(v) for v in box]
+                cv2.rectangle(frame, (x, y), (x + w, y + h),
+                    (0, 255, 0), 2)
+                centerX = np.rint((x + (w/2))).astype(int)
+                centerY = np.rint((y + (y/2))).astype(int)
+                #print(centerX,centerY)
+                if first:
+                    points = np.array([[centerX,centerY]],dtype=np.uint8)
+                    first=False
+                points = np.append(points,[[centerX,centerY]], axis=0)
+                cv2.polylines(frame, 
+                [points], 
+                isClosed = False,
+                color = (0,255,0),
+                thickness = 3, 
+                lineType = cv2.LINE_AA)
+            # update the FPS counter
+            fps.update()
+            fps.stop()
+            # initialize the set of information we'll be displaying on
+            # the frame
+            info = [
+                ("Tracker", args["tracker"]),
+                ("Success", "Yes" if success else "No"),
+                ("FPS", "{:.2f}".format(fps.fps())),
+            ]
+            # loop over the info tuples and draw them on our frame
+            for (i, (k, v)) in enumerate(info):
+                text = "{}: {}".format(k, v)
+                cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+        # show the output frame
+        #frame = cv2.flip(frame,1)
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+        # if the 's' key is selected, we are going to "select" a bounding
+        # box to track
+        if key == ord("s"):
+            print("s")
+            # select the bounding box of the object we want to track (make
+            # sure you press ENTER or SPACE after selecting the ROI)
+            initBB = cv2.selectROI("Frame", frame, fromCenter=False,
+                showCrosshair=True)
+            #when track movement don't use poseTracking
+            pose = False
+            # start OpenCV object tracker using the supplied bounding box
+            # coordinates
+            tracker.init(frame, initBB)
+                # if the `q` key was pressed, break from the loop
+        elif key == ord("q"):
+            break
+        # stop the timer and display FPS information
+    fps.stop()
+    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    fvs.stop()
